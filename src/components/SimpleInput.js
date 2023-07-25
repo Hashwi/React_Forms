edbced322cd24b332e958230859bdbf29495dfdb
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-07-25 12:18:59 +0200

    Adding_vallidation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,27 +1,46 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
-  const  nameInputRef = useRef();
-  const [enteredName, setEnteredName] = useState('');
-  
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
   const nameInputHandler = (event) => {
-    setEnteredName(event.target.value)
+    setEnteredName(event.target.value);
   };
 
   const formSubmissionHandler = (event) => {
-    event.preventDefault()
-    console.log(enteredName)
-    const enteredValue = nameInputRef.current.value
-    console.log(enteredValue)
-    setEnteredName('')
-  
+    event.preventDefault();
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    setEnteredName("");
   };
-  
+
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputHandler} value={enteredName} />
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputHandler}
+          value={enteredName}
+        />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name must Not be Empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
